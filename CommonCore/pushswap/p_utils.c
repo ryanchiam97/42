ea50a24885cc74c1c63ef4e9d981595c8a9b603e
Ryan Yiak Jin Chiam <rchiam@c1r1s6.42singapore.sg>
2025-08-16 16:32:10 +0800

    added stuff

--- CommonCore/pushswap/p_utils.c
+++ CommonCore/pushswap/p_utils.c
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   mainloop.c                                         :+:      :+:    :+:   */
+/*   p_utils.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rchiam <rchiam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/05 20:34:54 by rchiam            #+#    #+#             */
-/*   Updated: 2025/08/16 16:23:42 by rchiam           ###   ########.fr       */
+/*   Created: 2025/08/16 16:24:01 by rchiam            #+#    #+#             */
+/*   Updated: 2025/08/16 16:31:55 by rchiam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pushswap.h"
 
-int radixsort(t_stack *s1, t_stack *s2, int iter, char rdir)
+int	getrevdir(t_stack *s)
 {
-	//
+	int	top;
+	int	bottom;
+
+	top = 0;
+	bottom = s->size;
+	while()
+		//find the last '1' from the top? 
+		//... No better find firstfrom bottom/ first from top
+		//compare...
+		top++;
+	return (top <= bottom);
 }
