31f1a9b5bef8f7da649d1041d623214f6d4a0d3c
Ryan Yiak Jin Chiam <rchiam@c1r1s6.42singapore.sg>
2025-08-16 16:12:42 +0800

    sortfive fix, initstack.c added

--- CommonCore/pushswap/i_initstack.c
+++ CommonCore/pushswap/i_initstack.c
@@ -6,7 +6,7 @@
 /*   By: rchiam <rchiam@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/16 15:56:52 by rchiam            #+#    #+#             */
-/*   Updated: 2025/08/16 16:05:27 by rchiam           ###   ########.fr       */
+/*   Updated: 2025/08/16 16:12:05 by rchiam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,15 @@ t_stack	*initstack(int *arr, char name, int size, int maxsize)
 		s->push_to_other = pb;
 		s->push_to_self = pa;
 	}
+	else
+	{
+		s->swap = sb;
+		s->rotate = rb;
+		s->revrotate = rrb;
+		s->push_to_other = pa;
+		s->push_to_self = pb;
+	}
+	return (s);
 }
 
 void	loadstack(t_stack *s, t_stack *other)
